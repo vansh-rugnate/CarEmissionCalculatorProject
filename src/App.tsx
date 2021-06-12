f2c8e33e02d5f672ee4dbf8ba0cdc31d31363731
vansh-rugnate <vansh.rugnate123@outlook.com>
2021-06-12 19:55:31 +0100

    new code commit

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,18 @@ type cardetails = [string , number]
 const emissions: cardetails[] = [
     
     {
-      carmake:'audi',
-      co2: 150,
+      carmake:'volkswagen',
+      co2: 120, 
     },
-
+    
     {
-      carmake:'aston martin',
-      co2: 300,
+      carmake:'mercedes',
+      co2: 130, 
+    },
+    
+    {
+      carmake:'toyota',
+      co2: 108, 
     },
 
     {
@@ -38,6 +43,16 @@ const emissions: cardetails[] = [
       co2: 128,
     },
     
+    {
+      carmake:'aston martin',
+      co2: 300,
+    },    
+
+    {
+      carmake:'audi',
+      co2: 150,
+    },
+
     {
       carmake:'volkswagen',
       co2: 120, 
@@ -50,7 +65,127 @@ const emissions: cardetails[] = [
     
     {
       carmake:'toyota',
-      co2: 100, 
+      co2: 108, 
+    },
+
+    {
+      carmake:'bentley',
+      co2: 410, 
+    },
+
+    {
+      carmake:'bugatti',
+      co2: 540, 
+    },
+
+    {
+      carmake:'cadillac',
+      co2: 251, 
+    },
+
+    {
+      carmake:'chevrolet',
+      co2: 180, 
+    },
+    
+    {
+      carmake:'chrysler',
+      co2: 230, 
+    },
+    
+    {
+      carmake:'citroen',
+      co2: 106, 
+    },
+    
+    {
+      carmake:'dacia',
+      co2: 160, 
+    },
+    
+    {
+      carmake:'daf',
+      co2: 230, 
+    },
+    
+    {
+      carmake:'dodge',
+      co2: 210, 
+    },
+    
+    {
+      carmake:'ferarri',
+      co2: 350, 
+    },
+    
+    {
+      carmake:'fiat',
+      co2: 150, 
+    },
+    
+    {
+      carmake:'ford',
+      co2: 125, 
+    },
+    
+    {
+      carmake:'honda',
+      co2: 170, 
+    },  
+
+    {
+      carmake:'hyundai',
+      co2: 140, 
+    },
+
+    {
+      carmake:'jaguar',
+      co2: 200, 
+    },
+
+    {
+      carmake:'jeep',
+      co2: 250, 
+    },
+
+    {
+      carmake:'kia',
+      co2: 130, 
+    },
+
+    {
+      carmake:'lamborghini',
+      co2: 450, 
+    },
+
+    {
+      carmake:'lancia',
+      co2: 135, 
+    },
+
+    {
+      carmake:'land rover',
+      co2: 290, 
+    },
+
+    {
+      carmake:'lexus',
+      co2: 150, 
+    },
+
+    {
+      carmake:'maserati',
+      co2: 350, 
+    },
+
+    {
+      carmake:'mazda',
+      co2: 170, 
+    },
+
+    {
+      carmake:'mclaren',
+      co2: 271, 
     },
 
 ]
