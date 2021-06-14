11aaed0e0d2013133ed4c0c52775b9ce792ead1f
vansh-rugnate <vansh.rugnate123@outlook.com>
2021-06-14 23:21:54 +0100

    new code commit

--- src/App.tsx
+++ src/App.tsx
@@ -20,201 +20,162 @@ import './index.css';
 
 //make an array for the different cars with corresponding g/km
 
-// type cardetails = [ 
-  
-//   carmake:string, 
-  
-//   co2:number, 
-
-// ]
-
 let cars : { carmake: any; co2: any }[] = [
+    
   {
-  carmake:'vw',
-  co2:100,
+    carmake:'volkswagen',
+    co2: 120, 
   },
+  
   {
-  carmake:'merc', 
-  co2:110,
+    carmake:'mercedes',
+    co2: 130, 
   },
+  
   {
-  carmake:'audi', 
-  co2:120,
-  }
+    carmake:'toyota',
+    co2: 108, 
+  },
+
+  {
+    carmake:'alfa romeo',
+    co2: 128,
+  },
+  
+  {
+    carmake:'aston martin',
+    co2: 300,
+  },    
+
+  {
+    carmake:'audi',
+    co2: 150,
+  },
+
+  {
+    carmake:'bentley',
+    co2: 410, 
+  },
+
+  {
+    carmake:'bugatti',
+    co2: 540, 
+  },
+
+  {
+    carmake:'cadillac',
+    co2: 251, 
+  },
+
+  {
+    carmake:'chevrolet',
+    co2: 180, 
+  },
+  
+  {
+    carmake:'chrysler',
+    co2: 230, 
+  },
+  
+  {
+    carmake:'citroen',
+    co2: 106, 
+  },
+  
+  {
+    carmake:'dacia',
+    co2: 160, 
+  },
+  
+  {
+    carmake:'daf',
+    co2: 230, 
+  },
+  
+  {
+    carmake:'dodge',
+    co2: 210, 
+  },
+  
+  {
+    carmake:'ferarri',
+    co2: 350, 
+  },
+  
+  {
+    carmake:'fiat',
+    co2: 150, 
+  },
+  
+  {
+    carmake:'ford',
+    co2: 125, 
+  },
+  
+  {
+    carmake:'honda',
+    co2: 170, 
+  },  
+
+  {
+    carmake:'hyundai',
+    co2: 140, 
+  },
+
+  {
+    carmake:'jaguar',
+    co2: 200, 
+  },
+
+  {
+    carmake:'jeep',
+    co2: 250, 
+  },
+
+  {
+    carmake:'kia',
+    co2: 130, 
+  },
+
+  {
+    carmake:'lamborghini',
+    co2: 450, 
+  },
+
+  {
+    carmake:'lancia',
+    co2: 135, 
+  },
+
+  {
+    carmake:'land rover',
+    co2: 290, 
+  },
+
+  {
+    carmake:'lexus',
+    co2: 150, 
+  },
+
+  {
+    carmake:'maserati',
+    co2: 350, 
+  },
+
+  {
+    carmake:'mazda',
+    co2: 170, 
+  },
+
+  {
+    carmake:'mclaren',
+    co2: 271, 
+  },
+
 ];
 
-// emissions = ['w',5,'h',5];
+
     
-//     {
-//       carmake:'volkswagen',
-//       co2: 120, 
-//     },
-    
-//     {
-//       carmake:'mercedes',
-//       co2: 130, 
-//     },
-    
-//     {
-//       carmake:'toyota',
-//       co2: 108, 
-//     },
-
-//     {
-//       carmake:'alfa romeo',
-//       co2: 128,
-//     },
-    
-//     {
-//       carmake:'aston martin',
-//       co2: 300,
-//     },    
-
-//     {
-//       carmake:'audi',
-//       co2: 150,
-//     },
-
-//     {
-//       carmake:'volkswagen',
-//       co2: 120, 
-//     },
-    
-//     {
-//       carmake:'mercedes',
-//       co2: 130, 
-//     },
-    
-//     {
-//       carmake:'toyota',
-//       co2: 108, 
-//     },
-
-//     {
-//       carmake:'bentley',
-//       co2: 410, 
-//     },
-
-//     {
-//       carmake:'bugatti',
-//       co2: 540, 
-//     },
-
-//     {
-//       carmake:'cadillac',
-//       co2: 251, 
-//     },
-
-//     {
-//       carmake:'chevrolet',
-//       co2: 180, 
-//     },
-    
-//     {
-//       carmake:'chrysler',
-//       co2: 230, 
-//     },
-    
-//     {
-//       carmake:'citroen',
-//       co2: 106, 
-//     },
-    
-//     {
-//       carmake:'dacia',
-//       co2: 160, 
-//     },
-    
-//     {
-//       carmake:'daf',
-//       co2: 230, 
-//     },
-    
-//     {
-//       carmake:'dodge',
-//       co2: 210, 
-//     },
-    
-//     {
-//       carmake:'ferarri',
-//       co2: 350, 
-//     },
-    
-//     {
-//       carmake:'fiat',
-//       co2: 150, 
-//     },
-    
-//     {
-//       carmake:'ford',
-//       co2: 125, 
-//     },
-    
-//     {
-//       carmake:'honda',
-//       co2: 170, 
-//     },  
-
-//     {
-//       carmake:'hyundai',
-//       co2: 140, 
-//     },
-
-//     {
-//       carmake:'jaguar',
-//       co2: 200, 
-//     },
-
-//     {
-//       carmake:'jeep',
-//       co2: 250, 
-//     },
-
-//     {
-//       carmake:'kia',
-//       co2: 130, 
-//     },
-
-//     {
-//       carmake:'lamborghini',
-//       co2: 450, 
-//     },
-
-//     {
-//       carmake:'lancia',
-//       co2: 135, 
-//     },
-
-//     {
-//       carmake:'land rover',
-//       co2: 290, 
-//     },
-
-//     {
-//       carmake:'lexus',
-//       co2: 150, 
-//     },
-
-//     {
-//       carmake:'maserati',
-//       co2: 350, 
-//     },
-
-//     {
-//       carmake:'mazda',
-//       co2: 170, 
-//     },
-
-//     {
-//       carmake:'mclaren',
-//       co2: 271, 
-//     },
-
-// ]
-
-
-
-
 
 
 // start a const of App
