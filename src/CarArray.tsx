a0c1f733507dfbf57d3b756e776762fc2df907eb
vansh-rugnate <vansh.rugnate123@outlook.com>
2021-06-18 20:24:32 +0100

    dropbox makefield

--- src/CarArray.tsx
+++ src/CarArray.tsx
@@ -1,38 +1,38 @@
 export let cars : { carmake: string; co2: number }[] = [
     
   {
-    carmake:'volkswagen',
-    co2: 120, 
+    carmake:'abarth',
+    co2: 147.5,
   },
   
-  {
-    carmake:'mercedes',
-    co2: 130, 
-  },
-  
-  {
-    carmake:'toyota',
-    co2: 108, 
-  },
-
   {
     carmake:'alfa romeo',
-    co2: 128,
+    co2: 198.6 ,
   },
   
   {
     carmake:'aston martin',
-    co2: 300,
+    co2: 353.5,
   },    
 
   {
     carmake:'audi',
-    co2: 150,
+    co2: 183.3,
+  },
+
+  {
+    carmake:'audi sport',
+    co2: 183.5,
   },
 
   {
     carmake:'bentley',
-    co2: 410, 
+    co2: 391.1, 
+  },
+
+  {
+    carmake:'bmw',
+    co2: 172.6, 
   },
 
   {
@@ -48,6 +48,11 @@ export let cars : { carmake: string; co2: number }[] = [
   {
     carmake:'chevrolet',
     co2: 180, 
+  },
+
+  {
+    carmake:'chevrolet corvette',
+    co2: 190.9, 
   },
   
   {
@@ -87,7 +92,12 @@ export let cars : { carmake: string; co2: number }[] = [
   
   {
     carmake:'ford',
-    co2: 125, 
+    co2: 162.0, 
+  },
+
+  {
+    carmake:'ford mustang',
+    co2: 162.0, 
   },
   
   {
@@ -148,6 +158,141 @@ export let cars : { carmake: string; co2: number }[] = [
   {
     carmake:'mclaren',
     co2: 271, 
+  },
+
+  {
+    carmake:'mercedes amg',
+    co2: 192.6, 
+  },
+
+  {
+    carmake:'mercedes benz',
+    co2: 192.7, 
+  },
+
+  {
+    carmake:'mg',
+    co2: 227.7, 
+  },
+
+  {
+    carmake:'mini',
+    co2: 140.6, 
+  },
+
+  {
+    carmake:'mitsubishi',
+    co2: 200.9, 
+  },
+
+  {
+    carmake:'nissan gtr',
+    co2: 179.7, 
+  },
+
+  {
+    carmake:'nissan',
+    co2: 179.7 , 
+  },
+
+  {
+    carmake:'nissan nismo',
+    co2: 179.7, 
+  },
+
+  {
+    carmake:'perodua',
+    co2: 141.6, 
+  },
+
+  {
+    carmake:'peugeot',
+    co2: 154.0, 
+  },
+
+  {
+    carmake:'porsche',
+    co2: 246.9, 
+  },
+
+  {
+    carmake:'proton',
+    co2: 199.7, 
+  },
+
+  {
+    carmake:'renault',
+    co2: 166.6, 
+  },
+
+  {
+    carmake:'rolls royce',
+    co2: 382.3, 
+  },
+
+  {
+    carmake:'saab',
+    co2: 207.8, 
+  },
+
+  {
+    carmake:'seat',
+    co2: 146.7, 
+  },
+
+  {
+    carmake:'skoda',
+    co2: 149.3, 
+  },
+
+  {
+    carmake:'smart',
+    co2: 114.1, 
+  },
+
+  {
+    carmake:'ssangyong',
+    co2: 215.4, 
+  },
+
+  {
+    carmake:'subaru',
+    co2: 200.6, 
+  },
+
+  {
+    carmake:'suzuki',
+    co2: 168.0, 
+  },
+
+  {
+    carmake:'tata',
+    co2: 261.1, 
+  },
+
+  {
+    carmake:'tesla',
+    co2: 90, 
+  },
+
+  {
+    carmake:'toyota',
+    co2: 158.5, 
+  },
+
+  {
+    carmake:'vauxhall',
+    co2: 168.3, 
+  },
+
+  {
+    carmake:'volkswagen',
+    co2: 172.9, 
+  },
+
+  {
+    carmake:'volvo',
+    co2: 176.8, 
   },
 
 ];
--- src/CarEmissionCalculator.tsx
+++ src/CarEmissionCalculator.tsx
@@ -19,7 +19,7 @@ const CarEmissionsCalculator = () => {
     var lowermake = make.toLowerCase();
     
     if (make === '') {
-      alert('You have to enter a make!')
+      alert('You have to select a make!')
       return;
     }
 
@@ -67,20 +67,116 @@ const CarEmissionsCalculator = () => {
 
                 <div className='buttonpagebox1'>  
 
-                  <input
-                    value={make}
-                    onChange={(e) => setmake(e.target.value)}
-                    type="text"
-                    placeholder="Enter the make"
-                    className="input"
-                  />
+                  <select 
+                  
+                  id='make'
+                  name='make'
+                  onChange={(e) => setmake(e.target.value)}
+                  placeholder='Make'
+                  className='input'>
+
+                    <option value=''> Select a make... </option>
+                    
+                    <optgroup label='A' ></optgroup>
+                    <option value='abarth'> Abarth </option>
+                    <option value='alfa romeo'> Alfa Romeo </option>
+                    <option value='aston martin'> Aston Martin </option>
+                    <option value='audi'> Audi </option>
+                    <option value='audi sport'> Audi Sport </option>
+                    
+                    <optgroup label='B' ></optgroup>
+                    <option value='bentley'> Bentley </option>
+                    <option value='bmw'> BMW </option>
+                    <option value='bugatti'> Bugatti </option>
+                    
+                    <optgroup label='C' ></optgroup>
+                    <option value='cadillac'> Cadillac </option>
+                    <option value='chevrolet'> Chevrolet </option>
+                    <option value='chevrolet corvette'> Chevrolet Corvette </option>
+                    <option value='chrysler'> Chrysler </option>
+                    <option value='citroen'> Citroen </option>
+                    
+                    <optgroup label='D' ></optgroup>
+                    <option value='dacia'> Dacia </option>
+                    <option value='daf'> DAF </option>
+                    <option value='dodge'> Dodge </option>
+
+                    <optgroup label='F' ></optgroup>
+                    <option value='ferarri'> Ferarri </option>
+                    <option value='fiat'> Fiat </option>
+                    <option value='ford'> Ford </option>
+                    <option value='ford mustang'> Ford Mustang </option>
+
+                    <optgroup label='H' ></optgroup>
+                    <option value='honda'> Honda </option>
+                    <option value='hyundai'> Hyundai </option>
+
+                    <optgroup label='J' ></optgroup>
+                    <option value='jaguar'> Jaguar </option>
+                    <option value='jeep'> Jeep </option>
+
+                    <optgroup label='K' ></optgroup>
+                    <option value='kia'> Kia </option>
+
+                    <optgroup label='L' ></optgroup>
+                    <option value='lamborghini'> Lamborghini </option>
+                    <option value='lancia'> Lancia </option>
+                    <option value='land rover'> Land Rover </option>
+                    <option value='lexus'> Lexus </option>
+
+                    <optgroup label='M' ></optgroup>
+                    <option value='maserati'> Maserati </option>
+                    <option value='mazda'> Mazda </option>
+                    <option value='mclaren'> Mclaren </option>
+                    <option value='mercedes amg'> Mercedes AMG </option>
+                    <option value='mercedes benz'> Mercedes Benz </option>
+                    <option value='mg'> MG </option>
+                    <option value='mini'> Mini </option>
+                    <option value='mitsubishi'> Mitsubishi </option>
+
+                    <optgroup label='N' ></optgroup>
+                    <option value='nissan'> Mitsubishi </option>
+                    <option value='nissan gtr'> Mitsubishi </option>
+                    <option value='nissan nismo'> Mitsubishi </option>
+
+                    <optgroup label='P' ></optgroup>
+                    <option value='perodua'> Perodua </option>
+                    <option value='peugeot'> Peugeot </option>
+                    <option value='porsche'> Porsche </option>
+                    <option value='proton'> Proton </option>
+
+                    <optgroup label='R' ></optgroup>
+                    <option value='renault'> Renault </option>
+                    <option value='rolls royce'> Rolls-Royce </option>
+
+                    <optgroup label='S' ></optgroup>
+                    <option value='saab'> Saab </option>
+                    <option value='seat'> Seat </option>
+                    <option value='skoda'> Skoda </option>
+                    <option value='smart'> Smart </option>
+                    <option value='ssangyong'> Ssangyong </option>
+                    <option value='subaru'> Subaru </option>
+                    <option value='suzuki'> Suzuki </option>
+
+                    <optgroup label='T' ></optgroup>
+                    <option value='tata'> Tata </option>
+                    <option value='tesla'> Tesla </option>
+                    <option value='toyota'> Toyota </option>
+
+                    <optgroup label='V' ></optgroup>
+                    <option value='vauxhall'> Vauxhall </option>
+                    <option value='volkswagen'> Volkswagen </option>
+                    <option value='volvo'> Volvo </option>
+
+
+                  </select>
 
                   <input
                     value={mileage}
                     onChange={(e) => setmileage(e.target.value)}
-                    type="string"
-                    placeholder="Enter the mileage"
-                    className="input"
+                    type='string'
+                    placeholder='Enter the mileage'
+                    className='input'
                   />
 
                   <button type="submit" className="button">Submit</button>
